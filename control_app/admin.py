41ac49cff22d85baade493925d21cda1e8d2c564
FelipeNogueraP <117325494+FelipeNogueraP@users.noreply.github.com>
2023-02-11 02:20:00 +0000

    Tenant visualized

--- control_app/admin.py
+++ control_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tenant
 
 # Register your models here.
+
+class TenantAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields':['name','user_id']}),
+        ('Details', {'fields':['description','address']})
+        ]
+    list_display = ('name', 'user_id', 'description', 'address','id' )
+
+
+admin.site.register(Tenant, TenantAdmin)
