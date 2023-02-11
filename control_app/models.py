55f7e8fe591ab429d7f85745b0bce33939fe6194
FelipeNogueraP <117325494+FelipeNogueraP@users.noreply.github.com>
2023-02-11 00:29:10 +0000

    update .gitignore and add model tenant

--- control_app/models.py
+++ control_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Tenant(models.Model):
+    """Create the company user, aka tenant."""
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+    address = models.CharField(max_length=80)
